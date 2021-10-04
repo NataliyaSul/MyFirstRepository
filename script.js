a95e7ddca7e391f5289e26cc174bc1c404491cc2
Nataliya Sulyaeva <natali.nataliya.93@bk.ru>
2021-10-04 23:21:34 +0300

    lesson02fixed

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let title = "My project";
-console.log(typeof title);
-let screens = "простые , сложные , интерактивные , ";
+const title = "My project";
+const screens = "простые , сложные , интерактивные , ";
 let screenPrice = 20;
 let rollback = 16;
 let fullPrice = 1000;
+const adaptive = true;
+
+console.log(typeof title);
 console.log(typeof fullPrice);
-let adaptive = true;
 console.log(typeof adaptive);
 console.log(screens.length);
 console.log(screenPrice);
 console.log(fullPrice);
-
-console.log(screens.toLowerCase());
-console.log(screens.split(" "));
+console.log(screens.toLowerCase().split(" "));
 console.log(fullPrice * (rollback / 200));
