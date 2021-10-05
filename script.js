97f700aa45e63c93be27a7243db1d60b9429ba4a
Nataliya Sulyaeva <natali.nataliya.93@bk.ru>
2021-10-05 22:27:38 +0300

    style: исправление форматирования в lesson02

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const title = "My project";
-const screens = "простые , сложные , интерактивные , ";
+let screens = "простые, сложные, интерактивные";
 let screenPrice = 20;
 let rollback = 16;
 let fullPrice = 1000;
-const adaptive = true;
+let adaptive = true;
 
 console.log(typeof title);
 console.log(typeof fullPrice);
@@ -11,5 +11,5 @@ console.log(typeof adaptive);
 console.log(screens.length);
 console.log(screenPrice);
 console.log(fullPrice);
-console.log(screens.toLowerCase().split(" "));
+console.log(screens.toLowerCase().split(", "));
 console.log(fullPrice * (rollback / 200));
