1fee3fd2c162be53d8bad5e7fed896a4c926e903
Nataliya Sulyaeva <natali.nataliya.93@bk.ru>
2021-10-07 19:08:33 +0300

    lesson03 ready

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const title = "My project";
-let screens = "простые, сложные, интерактивные";
-let screenPrice = 20;
+'use strict';
+const title = prompt('Как называется ваш проект', "My project");
+console.log(title);
+let screens = prompt('Какие типы экранов нужно разработать?', "простые, сложные, интерактивные");
+console.log(screens);
+let screenPrice = prompt('Сколько будет стоить данная работа?', 20);
+console.log(screenPrice);
+// необходимо ли переводить число 20 из strict  в number ? в ТЗ этого просто не было
 let rollback = 16;
 let fullPrice = 1000;
-let adaptive = true;
+let adaptive = confirm("Нужен ли адаптив на сайте?", true);
+console.log(!!adaptive);
 
 console.log(typeof title);
 console.log(typeof fullPrice);
@@ -13,3 +19,32 @@ console.log(screenPrice);
 console.log(fullPrice);
 console.log(screens.toLowerCase().split(", "));
 console.log(fullPrice * (rollback / 200));
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service1);
+let servicePrice1 = prompt("Сколько это будет стоить?");
+console.log(servicePrice1);
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+console.log(service2);
+let servicePrice2 = prompt("Сколько это будет стоить?");
+console.log(servicePrice2);
+
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = fullPrice - rollback;
+console.log(Math.ceil(servicePercentPrice));
+
+if (fullPrice > 30000) {
+  console.log("скидка 10 %");
+}
+if (30000 > fullPrice > 15000) {
+  console.log("Даем скидку в 5%");
+}
+if (0 < fullPrice < 15000) {
+  console.log("Скидка не предусмотрена");
+}
+if (fullPrice < 0) {
+  console.log("Что то пошло не так");
+}
+
+
